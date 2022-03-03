4cc266e10d849e9a77cd89b6ad2b0e421060b4dd
Maxon Crumb <jmcrumb3@gmail.com>
2022-03-03 14:29:29 -0800

    Refactor core into python

---
+++ nova-core/core/abstract_plugin.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractclassmethod
+
+class NovaPlugin(ABC):
+
+    def __init__(self):
+        pass
+    
+    @abstractclassmethod
+    def get_keywords(self) -> list:
+        pass
+
+    @abstractclassmethod
+    def execute(self, command: str)-> str:
+        pass
+
+    @abstractclassmethod
+    def execute_secondary_command(self, command: str) -> str:
+        pass
+
+    def request_history(self) -> list:
+        return [];
+    
+    def __string__(self) -> str: 
+        return __class__.__name__
---
+++ nova-core/nlp/nlp.py
@@ -0,0 +1,5 @@
+def speech_to_text(input_: any) -> str:
+    return input_
+
+def text_to_speech(input_: str) -> any:
+    return input_
