cfa25ecc162e862607c4579abd196d34b174d7a0
kananb <kananboubion@gmail.com>
2022-03-15 18:23:06 -0700

    Abstract more functionality into apitest package

--- back-end/src/nova/plugins/plugins_test.go
+++ back-end/src/nova/plugins/plugins_test.go
@@ -1,18 +1,13 @@
 package plugins
 
 import (
-	"encoding/json"
 	"net/http"
-	"net/http/httptest"
 	"os"
-	"reflect"
-	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jmcrumb/nova/apitest"
 	"github.com/jmcrumb/nova/database"
-	"github.com/stretchr/testify/assert"
 )
 
 var router *gin.Engine
@@ -73,38 +68,30 @@ func TestPostPlugin(t *testing.T) {
 		},
 	}
 
-	for _, test := range tests {
-		// perform request
-		w := httptest.NewRecorder()
-		var body string
-		if reflect.TypeOf(test.Body) == reflect.TypeOf("") {
-			body = test.Body.(string)
-		} else {
-			marshalled, _ := json.Marshal(&test.Body)
-			body = string(marshalled)
-		}
-		req, _ := http.NewRequest("POST", "/plugin/", strings.NewReader(body))
-		router.ServeHTTP(w, req)
+	apitest.TryRequests(apitest.APITestArgs{
+		T:      t,
+		Router: router,
+		Tests:  tests,
 
-		// check http result values
-		assert.Equal(t, test.Status, w.Code)
-		if w.Code != http.StatusCreated {
-			assert.Equal(t, test.Err, w.Body.String())
-		}
+		Method: "POST",
+		URL:    "/plugin/",
 
-		// check database
-		var plugins []database.Plugin
-		err := database.DB.Table("plugin").Find(&plugins).Error
-		if err != nil {
-			t.Errorf("%v", err)
-		}
+		QueryRows: func() []interface{} {
+			var plugins []database.Plugin
+			err := database.DB.Table("plugin").Find(&plugins).Error
+			if err != nil {
+				t.Errorf("%v", err)
+			}
 
-		var rows []interface{}
-		for _, plugin := range plugins {
-			rows = append(rows, plugin)
-		}
-		apitest.AssertResultsEqual(t, test.Rows, rows, comparePlugins)
-	}
+			var rows []interface{}
+			for _, plugin := range plugins {
+				rows = append(rows, plugin)
+			}
+
+			return rows
+		},
+		Comparator: comparePlugins,
+	})
 }
 
 func TestPutPlugin(t *testing.T) {
