97135fccd4e8a13b0c850264fdd96ffa53a00b9d
Maxon Crumb <jmcrumb3@gmail.com>
2022-03-20 13:30:35 -0700

    Nova responds and listens verbally

--- nova-core/main.py
+++ nova-core/main.py
@@ -1,16 +1,41 @@
 import signal
 from core.nova_core import NovaCore
-
-def response_handler(response: any):
-    print(f'\nNova> {response}\nUser> ', end='')
+from nlp.nlp import SpeechRecognition
 
 def main():
-    core = NovaCore(response_handler)
-    # signal.signal(signal.SIG)
-    
+    # Sets text only CLI interface
+    TEXT_DEBUG = False
+
+    if TEXT_DEBUG:
+        core = NovaCore(text_only_response_handler)
+    else:
+        core = NovaCore(audio_response_handler)
+
+    # Set voice [OPTIONAL]
+    sr: SpeechRecognition = SpeechRecognition()
+    sr.set_voice(17)
+
+    # Input loop
     while True:
-        command: str = input('User> ')
+        if TEXT_DEBUG:
+            command: str = text_only_input_handler()
+        else:
+            command: str = microphone_input_handler()
         core.invoke(command)
+
+def text_only_response_handler(response: str):
+    print(f'\nNova> {response}\nUser> ', end='')
+
+def text_only_input_handler() -> str:
+    return input('User> ')
+
+def microphone_input_handler() -> str:
+    sr: SpeechRecognition = SpeechRecognition()
+    return sr.speech_to_text()
+
+def audio_response_handler(response: str):
+    sr: SpeechRecognition = SpeechRecognition()
+    sr.text_to_speech(response)
 
 if __name__ =='__main__':
     main()
--- nova-core/nlp/nlp.py
+++ nova-core/nlp/nlp.py
@@ -1,5 +1,51 @@
-def speech_to_text(input_: any) -> str:
-    return input_
+from threading import Semaphore
 
-def text_to_speech(input_: str) -> any:
-    return input_
+import speech_recognition as sr
+import pyttsx3
+
+class SpeechRecognition:
+    '''Singleton implmentation of Speech Recognition utility functions'''
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SpeechRecognition, cls).__new__(cls)
+            cls._instance._initialization_routine()
+        return cls._instance
+
+    def _initialization_routine(self) -> None:
+        self.recognizer = sr.Recognizer()
+        self.tts_engine = pyttsx3.init()
+        self._tts_mutex = Semaphore()
+
+
+    def set_voice(self, voice_index: int) -> None:
+        voices = self.tts_engine.getProperty('voices')
+        self.tts_engine.setProperty('voice', voices[voice_index].id)
+
+    def speech_to_text(self, input_: str) -> str:
+        '''Speech to text which accepts str or .wav'''
+        return input_
+  
+    
+    def speech_to_text(self) -> str:
+         with sr.Microphone() as source:
+            self.recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            print('Say something')
+            audio_data = self.recognizer.listen(source)
+            print('processing')
+            text: str = self.recognizer.recognize_google(audio_data)
+            print(text)
+            return text
+    
+
+    def text_to_speech(self, input_: str, language='en') -> any:
+        self._tts_mutex.acquire()
+        # if self.tts_engine._inLoop:
+        #     self.tts_engine.endLoop()
+        self.tts_engine.say(input_)
+        self.tts_engine.runAndWait()
+        self.tts_engine.endLoop()   # add this line
+        self.tts_engine.stop()
+        self._tts_mutex.release()
