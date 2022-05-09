17ad265ce6bcef39fe07c1715d1fd9b823bfabf6
kananb <kananboubion@gmail.com>
2022-05-09 11:53:40 -0700

    Implement home view

--- front-end/nova-website/src/App.tsx
+++ front-end/nova-website/src/App.tsx
@@ -4,6 +4,7 @@ import "./styles.scss";
 import "./components/PluginList.css"
 import "./components/PluginPreview.css"
 import "./components/Rating.scss"
+import "./components/Home.css"
 import { ReactQueryDevtools } from "react-query/devtools";
 import Navbar from "./components/nav";
 import { BrowserRouter } from "react-router-dom";
--- front-end/nova-website/src/components/sandbox.tsx
+++ front-end/nova-website/src/components/sandbox.tsx
@@ -4,6 +4,7 @@ import { PluginViewPublic } from "./plugin";
 import PluginSearch from "./PluginSearch";
 import PublishedPlugins from "./PublishedPlugins";
 import Login from "./Login";
+import Home from "./Home";
 
 export default function Sandbox() {
 
@@ -12,7 +13,8 @@ export default function Sandbox() {
       {/* <Example /> */}
       {/* <PluginViewPublic id={pluginID} /> */}
 	  {/* <PublishedPlugins query="86ee5cd6-5c83-4fd3-b4d6-1c2064dcd918" /> */}
-	  <Login />
+	  {/* <Login /> */}
+	  <Home />
     </Container>
   );
 }
