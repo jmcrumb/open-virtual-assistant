4465ca332eb169bc32272ca7e47d38610bffebf3
kananb <kananboubion@gmail.com>
2022-05-09 18:49:39 -0700

    Add API call to plugin publish view

--- front-end/nova-website/src/components/PublishPlugin.tsx
+++ front-end/nova-website/src/components/PublishPlugin.tsx
@@ -1,5 +1,8 @@
-import { useTheme } from "@emotion/react";
+import { BACKEND_SRC } from "../api/helper";
+import { Review, Plugin } from "../api/pluginStoreAPI";
+import axios from "axios";
 import * as React from "react";
+import UserState from "../userState"
 
 function InputField(props) {
 	const {name, label, placeholder, lines, charLimit} = props
@@ -43,13 +46,32 @@ function InputField(props) {
 }
 
 function PublishPlugin() {
+	const sourceLink = React.useRef(null)
+	const pluginName = React.useRef(null)
+	const description = React.useRef(null)
+
+	const getInputText = input => {
+		return input.current.querySelector(".field").value
+	}
+	const onPublish = () => {
+		let plugin = new Plugin({
+			"publisher": UserState.getInstance().state["id"],
+			"name": getInputText(sourceLink),
+			"sourceLink": getInputText(pluginName),
+			"about": getInputText(description),
+		})
+		axios.post(`${BACKEND_SRC}plugin`, plugin)
+		// need error handling and feedback to user about whether
+		// the request went through or not
+	}
+
 	return (
 		<div className="PublishPlugin">
 			<h1>Publish a new plugin</h1>
-			<InputField name="source-link" label="link source code" lines={1} placeholder="source code link" />
-			<InputField name="plugin-name" label="name your plugin" lines={1} placeholder="plugin name" />
-			<InputField name="description" label="give it a description" lines={5} charLimit={500} placeholder="description" />
-			<button className="publish">publish</button>
+			<InputField ref={sourceLink} name="source-link" label="link source code" lines={1} placeholder="source code link" />
+			<InputField ref={pluginName} name="plugin-name" label="name your plugin" lines={1} placeholder="plugin name" />
+			<InputField ref={description} name="description" label="give it a description" lines={5} charLimit={500} placeholder="description" />
+			<button className="publish" onClick={onPublish}>publish</button>
 		</div>
 	)
 }
