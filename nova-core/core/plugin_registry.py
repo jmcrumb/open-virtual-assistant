4cf85bf2d26e7be99fe5f799c4eec7473e7d22e3
joehartUSD <joehart@sandiego.edu>
2022-05-13 12:42:41 -0700

    Adds pogger levels of function

--- nova-core/core/plugin_registry.py
+++ nova-core/core/plugin_registry.py
@@ -1,8 +1,10 @@
 from plugins.hello_world_plugin import HelloWorldPlugin
 from plugins.weather_plugin import WeatherPlugin
 from plugins.time_plugin import TimePlugin
+from plugins.wiki_plugin import WikiPlugin
 registry=[
 	HelloWorldPlugin,
 	WeatherPlugin,
 	TimePlugin,
+	WikiPlugin,
 ]
--- nova-core/plugins/wiki_plugin.py
+++ nova-core/plugins/wiki_plugin.py
@@ -10,11 +10,16 @@ class WikiPlugin(NovaPlugin):
         if ('wiki' in command):
             wiki_wiki = wikipediaapi.Wikipedia('en')
 
-            page_py = wiki_wiki.page('Python_(programming_language)')
-            print(page_py.exists())
-            return 'It is currently {}.'.format(datetime.now().strftime("%H:%M"))
+            item = command[command.rfind("wiki")+4:].strip()
+            page = wiki_wiki.page(item)
+            if page.exists():
+                msg = (page.summary[0:page.summary.find('.', page.summary.find('.')+1)])
+            else:
+                msg = ('{} not found. Please try again.'.format(item))
+        else:
+            msg = None
         
-        return None
+        return msg
 
     def help_command(self, command: str) -> str:
-        return 'This plugin returns the current time to the user.'
+        return 'This plugin returns the wiki information to the user.'
