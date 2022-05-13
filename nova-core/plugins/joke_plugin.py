69252b1e38801671e24f17da5efcfd3489bbebcf
joehartUSD <joehart@sandiego.edu>
2022-05-13 14:29:54 -0700

    joke plugin

--- nova-core/plugins/joke_plugin.py
+++ nova-core/plugins/joke_plugin.py
@@ -3,8 +3,11 @@ from core.abstract_plugin import NovaPlugin
 class JokePlugin(NovaPlugin):
 	def __init__(self):
 		self.jokes = [
-			"What do you call a person with no arms or legs? ... a cripple!",
-			"Three men walk into a bar, the first one gets a drink, the second sees someone he knows, and the third immediately sexually harasses a girl and is arrested the end."
+			"What do you call a cow with no legs? ... ground beef!",
+			"What do you call a fake noodle? ... an im-pasta!",
+			"Why can't you trust atoms? ... they make up everything!",
+			"How much does a polar bear weight? ... enough to break the ice!",
+			"Why should I tell you a joke? You're the clown here!"
 		]
 		self.last = 0
 
@@ -12,11 +15,14 @@ class JokePlugin(NovaPlugin):
 		return ['joke', 'funny']
 
 	def execute(self, command: str) -> str:
-		self.last += 1
-		if self.last > len(self.jokes):
-			self.last = 0
+		if ('joke' in command or 'funny' in command or 'another' in command):
+			self.last += 1
+			if self.last >= len(self.jokes):
+				self.last = 0
 
-		return self.jokes[self.last]
+			return self.jokes[self.last]
+		
+		return None
 
 	def help_command(self, command: str) -> str:
 		return ('This joke plugin is powered by the pure comedic genius of Kanan Boubion.'
